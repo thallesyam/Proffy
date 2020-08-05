38af2baa96d8d84477ec65d07a83dd0df5a4890d
Thalles Ian <thallesyam@users.=.github.com>
2020-08-04 21:20:07 -0300

    desenvolvendo o front-end em react, configurando rotas e estilizando pagina principal e de procura

---
+++ src/components/TeacherItem/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+/* ICONS */
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
+
+/* CSS */
+import './styles.css'
+
+function TeacherItem() {
+  return (
+    <article className="teacher-item">
+      <header>
+        <img src="https://avatars1.githubusercontent.com/u/59545660?s=460&u=90f09bcadd23faf9ab59697ccc78d81a43e64102&v=4" alt="Thalles Ian"/>
+        <div>
+          <strong>Thalles Ian</strong>
+          <span>Programação</span>
+        </div>
+      </header>
+      <p>
+        Entusiasta das melhores tecnologias do mercado!
+        <br></br>
+        Apaixonado por criar coisas de visual bonito e com uma funcionalidade agradavel
+        Design um dia também, quem sabe
+      </p>
+      <footer>
+        <p>
+          Preço/hora
+          <strong>R$ 50,00</strong>
+        </p>
+        <button type="button">
+          <img src={whatsappIcon} alt="WhatsApp"/>
+          Entrar em contato
+        </button>
+      </footer>
+    </article>
+  )
+}
+
+export default TeacherItem
